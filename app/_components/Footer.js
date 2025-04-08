d43d692e5050fcb25fa82b9217ac71efba8517e1
jarrenleo <jarren.leoo@gmail.com>
2025-04-08 21:33:11 +0800

    changes

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
 
   return (
     <footer className="border-t-[1px] border-border py-8 text-center text-sm text-muted-foreground sm:text-right">
-      &copy; {currentYear} Nicholas Ong. All rights reserved.
+      &copy; {currentYear} Nicholas Ong. All Rights Reserved.
     </footer>
   );
 }
